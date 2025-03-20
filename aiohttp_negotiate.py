78297614f3e1e8e91e423476b8a47bf00291c064
silviavandenbogaartmarzola <marzolasilvia3@gmail.com>
2025-03-20 09:41:35 +0100

    Reformat

--- aiohttp_negotiate.py
+++ aiohttp_negotiate.py
@@ -12,11 +12,14 @@ logger = logging.getLogger(__name__)
 
 
 class NegotiateMixin(object):
-    def __init__(self, *,
-                 negotiate_client_name=None,
-                 negotiate_service_name=None,
-                 negotiate_service='HTTP',
-                 **kwargs):
+    def __init__(
+        self,
+        *,
+        negotiate_client_name=None,
+        negotiate_service_name=None,
+        negotiate_service="HTTP",
+        **kwargs
+    ):
         self.negotiate_client_name = negotiate_client_name
         self.negotiate_service_name = negotiate_service_name
         self.negotiate_service = negotiate_service
@@ -26,30 +29,33 @@ class NegotiateMixin(object):
     def get_hostname(response):
         assert isinstance(response, aiohttp.ClientResponse)
         assert isinstance(response.connection, aiohttp.connector.Connection)
-        sock = response.connection._transport.get_extra_info('socket')
+        sock = response.connection._transport.get_extra_info("socket")
         assert isinstance(sock, (ssl.SSLSocket, socket.socket))
         return socket.gethostbyaddr(sock.getpeername()[0])[0]
 
     def get_context(self, host):
-        service_name = gssapi.Name(self.negotiate_service_name or '{0}@{1}'.format(self.negotiate_service, host),
-                                   gssapi.NameType.hostbased_service)
+        service_name = gssapi.Name(
+            self.negotiate_service_name
+            or "{0}@{1}".format(self.negotiate_service, host),
+            gssapi.NameType.hostbased_service,
+        )
         logger.debug("Service name: {0}".format(service_name))
         if self.negotiate_client_name:
-            creds = gssapi.Credentials(name=gssapi.Name(self.negotiate_client_name),
-                                       usage='initiate')
+            creds = gssapi.Credentials(
+                name=gssapi.Name(self.negotiate_client_name), usage="initiate"
+            )
         else:
             creds = None
         logger.debug("Credentials: {0}".format(creds))
-        return gssapi.SecurityContext(name=service_name,
-                                      creds=creds)
+        return gssapi.SecurityContext(name=service_name, creds=creds)
 
     @staticmethod
     def get_challenges(response):
         challenges = {}
         for k, v in response.headers.items():
-             if k.lower() == 'www-authenticate':
-                 challenges.update(www_authenticate.parse(v))
-        logger.debug('Server challenges: {}'.format(challenges))
+            if k.lower() == "www-authenticate":
+                challenges.update(www_authenticate.parse(v))
+        logger.debug("Server challenges: {}".format(challenges))
         return challenges
 
     @staticmethod
@@ -58,29 +64,33 @@ class NegotiateMixin(object):
             in_token = base64.b64decode(in_token)
         out_token = ctx.step(in_token)
         if out_token:
-            out_token = base64.b64encode(out_token).decode('utf-8')
+            out_token = base64.b64encode(out_token).decode("utf-8")
         return out_token
 
     async def _request(self, method, url, *, headers=None, **kwargs):
         headers = headers or {}
         response = await super()._request(method, url, headers=headers, **kwargs)
         challenges = self.get_challenges(response)
-        if response.status == UNAUTHORIZED and 'negotiate' in challenges:
+        if response.status == UNAUTHORIZED and "negotiate" in challenges:
             host = self.get_hostname(response)
             ctx = self.get_context(host)
             out_token = self.negotiate_step(ctx)
             while True:
                 response.close()
                 if out_token:
-                    headers['Authorization'] = 'Negotiate ' + out_token
-                    response = await super()._request(method, url, headers=headers, **kwargs)
-                challenges = www_authenticate.parse(response.headers.get('WWW-Authenticate'))
-                in_token = challenges['negotiate']
+                    headers["Authorization"] = "Negotiate " + out_token
+                    response = await super()._request(
+                        method, url, headers=headers, **kwargs
+                    )
+                challenges = www_authenticate.parse(
+                    response.headers.get("WWW-Authenticate")
+                )
+                in_token = challenges["negotiate"]
                 self.negotiate_step(ctx, in_token)
                 if ctx.complete:
                     break
         return response
 
+
 class NegotiateClientSession(NegotiateMixin, aiohttp.ClientSession):
     pass
-
